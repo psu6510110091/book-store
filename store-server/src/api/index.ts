9700b9253077193b7152152befdfb888e695ec92
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2022-12-16 18:18:52 +0700

    import ให้สามารถเรียก book ได้

--- store-server/src/api/index.ts
+++ store-server/src/api/index.ts
@@ -1,7 +1,9 @@
 import Router from 'koa-router'
 import category from './category'
+import book from './book'
 
 const router = new Router()
 router.use('/api/category',category.routes())
+router.use('/api/book', book.routes())
 
 export default router
