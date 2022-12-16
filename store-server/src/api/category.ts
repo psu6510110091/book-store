6df81236194fc4d88ca8b645542ac7618f846ac7
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2022-12-16 11:10:57 +0700

    เชื่อมต่อ api category

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -1,6 +1,9 @@
 import Router from "koa-router";
+import db from '../db';
 
 const router = new Router()
+const makeQuery = () => db('category').select('*')
+const findById = (id: number) => makeQuery().where({id})
 
 router.get('/', async (ctx, next) => {
   ctx.body = [
