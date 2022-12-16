24c9098d09417b96108d8ea33b53ff28e662b95d
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2022-12-16 11:51:43 +0700

    เพิ่ม method ใน api/category

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -5,11 +5,18 @@ const router = new Router()
 const makeQuery = () => db('category').select('*')
 const findById = (id: number) => makeQuery().where({id})
 
-router.get('/', async (ctx, next) => {
-  ctx.body = [
-    {id: 1, title: 'Programming'},
-    {id: 2, title: 'Life Style'}
-  ]
-})
+router
+  .get('/', async (ctx, next) => {
+    ctx.body = await makeQuery().orderBy('id')
+  })
+  .get('/:id', async (ctx, next) => {
+    const id = parseInt(ctx.params.id)
+    const category = await findById(id).first()
+    if(!category){
+      ctx.response.status = 404
+    }
+    ctx.body = category
+  })
+
 
 export default router
