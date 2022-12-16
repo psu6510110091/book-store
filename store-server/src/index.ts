cba10ee5f09a12a9218ec45d3f5aca7078e29034
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2022-12-16 15:52:42 +0700

    import modules koa

--- store-server/src/index.ts
+++ store-server/src/index.ts
@@ -1,10 +1,14 @@
 import Koa from 'koa'
-import json from 'koa-json'
 import api from './api'
+import json from 'koa-json'
+import { koaBody } from 'koa-body'
+import cors from '@koa/cors'
 
 const app = new Koa()
 
 app.use(json())
+app.use(cors())
+app.use(koaBody())
 app.use(api.routes())
 
 app.listen(8000)
