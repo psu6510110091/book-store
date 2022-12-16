ded796ded476a9a1ffb756dd32d8d47473c99b8d
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2022-12-16 23:42:10 +0700

    import Book Repository

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState, useRef } from 'react'
+import BookDetail from './components/BookDetail'
+import Book from './models/Book'
 import Category from './models/Category'
 import Repo from './repositories'
 
 function App() {
+  const [bookList, setBookList] = useState<Book[]>([])
   const [categoryList, setCategoryList] = useState<Category[]>([])
-
+  const fetchBookList = async () => {
+    const result = await Repo.books.getAll()
+    if (result) {
+      setBookList(result)
+    }
+  }
   const fetchCategoryList = async () => {
     const result = await Repo.categories.getAll()
     if (result) {
@@ -14,11 +22,24 @@ function App() {
 
   useEffect(() => {
     fetchCategoryList()
+    fetchBookList()
   }, [])
 
   return (
     <div>
-      {categoryList.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
+      <div>
+        {categoryList.map(category => 
+          <option key={category.id} value={category.id}> {category.title}</option>
+        )}
+        <hr />
+      </div>
+      
+      {bookList.map( book => 
+        <div key={book.id}>
+          <BookDetail {...book}/> 
+          <hr />
+        </div>
+      )}
     </div> 
   );
 }
--- store-client/src/repositories/index.ts
+++ store-client/src/repositories/index.ts
@@ -1,6 +1,8 @@
+import { BookRepository } from "./BookRepository"
 import { CategoryRepository } from "./CategoryRepository"
 
 const repositories = {
+  books: new BookRepository(),
   categories: new CategoryRepository()
 }
 
